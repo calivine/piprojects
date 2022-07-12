7f08110b9c8ac857facc8a57d66f10008b99ba48
Alex Caloggero <a.calog@protonmail.com>
2022-07-12 19:10:53 -0400

    2 LED program

---
+++ lights.py
@@ -0,0 +1,18 @@
+from time import sleep
+
+from led import Led
+
+red = Led(4)
+
+green = Led(5)
+
+while True:
+    red.turn_on()
+    sleep(5)
+    red.turn_off()
+    sleep(5)
+    green.turn_on()
+    sleep(5)
+    green.turn_off()
+
+
