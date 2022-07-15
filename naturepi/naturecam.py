169bab27bb98129418905eaf35071b717e5d2912
Alex Caloggero <a.calog@protonmail.com>
2022-07-14 21:36:07 -0400

    naturepi module

---
+++ naturepi/naturecam.py
@@ -0,0 +1,36 @@
+from time import sleep,strftime,localtime
+
+from gpiozero import MotionSensor
+from camera import Camera
+from led import Led
+
+
+class NatureCam:
+
+    def __init__(self):
+        self.pir = MotionSensor(27)
+        self.red = Led(4)
+        self.green = Led(17)
+        self.camera = Camera(resolution=(640, 480))
+        self.pir.when_motion = self._recording
+        self.pir.when_no_motion = self._standby
+        self.green.turn_on()
+
+    def _standby(self):
+        print("3 standby")
+        self.green.turn_on()
+        self.red.turn_off()
+
+    def _recording(self):
+        print("2 recording")
+        self.green.turn_off()
+        self.red.turn_on()
+        self.camera.capture()
+        self.camera.record(strftime("%A-%d-%B-%Y_%H_%M_%S", localtime()))
+        sleep(2)
+
+    def activate(self):
+        while True:
+            print("1 Waiting for motion")
+            self.pir.wait_for_motion()
+            sleep(8)
---
+++ naturepi/naturepi.py
@@ -0,0 +1,14 @@
+import sys
+
+from camera import Camera
+from naturecam import NatureCam
+
+
+def _real_main():
+    NatureCam().activate()
+
+
+try:
+    _real_main()
+except KeyboardInterrupt:
+    sys.exit('\nERROR: Interrupted by user')
